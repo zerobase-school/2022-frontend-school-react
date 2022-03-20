e12cb79be3e4bdefe06c3e212262444295796325
zerobase <school@zero-base.co.kr>
2022-03-20 16:56:52 +0900

    feat: 1~5강 summary mission

---
+++ 5/cards.js
@@ -0,0 +1,74 @@
+const cards = [
+  {
+    company: "오카카",
+    team: "개발팀",
+    name: "클로이",
+    phoneNumber: "010-1111-2222",
+    email: "chloe@ohkaka.com",
+  },
+  {
+    company: "버이네",
+    team: "마케팅팀",
+    name: "토드",
+    phoneNumber: "010-1112-2222",
+    email: "tod@verleene.com",
+  },
+  {
+    company: "가지마켓",
+    team: "디자인팀",
+    name: "제나",
+    phoneNumber: "010-1113-2222",
+    email: "jena@eggplant.com",
+  },
+  {
+    company: "집밥의민족",
+    team: "개발팀",
+    name: "조던",
+    phoneNumber: "010-1114-2222",
+    email: "jordan@homecook.com",
+  },
+  {
+    company: "코팡",
+    team: "총무팀",
+    name: "헤일리",
+    phoneNumber: "010-1115-2222",
+    email: "haley@copang.com",
+  },
+  {
+    company: "퀼슨",
+    team: "컨텐츠팀",
+    name: "그레이",
+    phoneNumber: "010-1116-2222",
+    email: "grey@quilson.com",
+  },
+  {
+    company: "내일의집",
+    team: "기획팀",
+    name: "제임스",
+    phoneNumber: "010-1117-2222",
+    email: "james@tomorrowhome.com",
+  },
+  {
+    company: "방직",
+    team: "사업전략팀",
+    name: "미셸",
+    phoneNumber: "010-1118-2222",
+    email: "michelle@bang.com",
+  },
+  {
+    company: "투스",
+    team: "피플팀",
+    name: "가든",
+    phoneNumber: "010-1119-2222",
+    email: "garden@touss.com",
+  },
+  {
+    company: "브로컬리",
+    team: "브랜딩팀",
+    name: "톰",
+    phoneNumber: "010-1120-2222",
+    email: "kim@broccoli.com",
+  },
+];
+
+export default cards;
